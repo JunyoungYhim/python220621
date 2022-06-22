08f2cbb82e66397210db0c0fbed98e84172d00c7
JunyoungYhim <junyoung.yhim@gmail.com>
2022-06-22 16:14:58 +0900

    연습완료

---
+++ db1.py
@@ -0,0 +1,33 @@
+# db1.py
+
+import sqlite3
+
+#연결 객체를 리턴받기
+con = sqlite3.connect(":memory:")
+#커서 객체를 리턴받기
+cur = con.cursor()
+#테이블(스키마)를 생성
+cur.execute("create table PhoneBook (Name text, PhoneNum text);")
+#1건 입력
+cur.execute("insert into PhoneBook values ('derick', '010-222');")
+
+#입력 파라미터 처리
+name = "gildong"
+phoneNumber = "010-123"
+cur.execute("insert into PhoneBook values (?,?);", (name, phoneNumber))
+
+#여러건을 입력
+datalist = (("tom","011-123"), ("dsp","010-456"))
+cur.executemany("insert into PhoneBook values (?,?);", datalist)
+
+#검색
+cur.execute("select * from PhoneBook;") # 폰북테이블의 조건없이 모든 컬럼을 가져와
+
+# for row in cur:
+#     print(row)
+
+#이렇게도 출력해서 볼 수 있다.
+for row in cur:
+    print(row[0] + "," + row[1])
+    
+
