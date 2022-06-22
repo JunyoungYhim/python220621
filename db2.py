378b5c6e598038ed4519181f91836f8515d518f8
JunyoungYhim <junyoung.yhim@gmail.com>
2022-06-22 16:44:31 +0900

    완료

--- db2.py
+++ db2.py
@@ -1,9 +1,9 @@
-# db1.py
+# db2.py
 
 import sqlite3
 
-#연결 객체를 리턴받기
-con = sqlite3.connect(":memory:")
+#연결 객체를 리턴받기(파일에 영구적으로 저장)
+con = sqlite3.connect("c:\\work\\sample.db")
 #커서 객체를 리턴받기
 cur = con.cursor()
 #테이블(스키마)를 생성
@@ -21,21 +21,10 @@ datalist = (("tom","011-123"), ("dsp","010-456"))
 cur.executemany("insert into PhoneBook values (?,?);", datalist)
 
 #검색
-cur.execute("select * from PhoneBook;") # 폰북테이블의 조건없이 모든 컬럼을 가져와
-
-#검색 매서드 사용
-print("---fetchone()---")
-print(cur.fetchone())
-print("---fetchmany(2)---")
-print(cur.fetchmany(2))
-print("---fetchall()---")
-print(cur.fetchall())
-# 1건 2,건 4건이 나오는게 아니라 이미 가져온거는 가져오는 대상에서 빠짐
-# 때문에 fetchall을 하더라도 마지막 데이터 1개만 가져오게 된다.
-
-print("---fetchall()---")
 cur.execute("select * from PhoneBook;")
 print(cur.fetchall())
+# 작업을 정상적으로 완료
+con.commit()
 
 # for row in cur:
 #     print(row)
